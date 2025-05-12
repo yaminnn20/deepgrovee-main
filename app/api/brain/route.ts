b5606d906a89bf8f65de0e302c2fde66d09b4f73
shifttt <yamin969788@gmail.com>
2025-05-12 11:40:10 +0530

    Revert "first2"
    This reverts commit f81748a53ff70c8b4835d3f135af5f059c18bcee.

--- app/api/brain/route.ts
+++ app/api/brain/route.ts
@@ -2,17 +2,20 @@ import OpenAI from "openai";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 
 // Optional, but recommended: run on the edge runtime.
+// See https://vercel.com/docs/concepts/functions/edge-functions
 export const runtime = "edge";
 
+const client = new OpenAI({
+  apiKey: process.env.GROQ_API_KEY,
+  baseURL: "https://api.groq.com/openai/v1"
+});
+
 export async function POST(req: Request) {
+  // Extract the `messages` from the body of the request
   const { messages } = await req.json();
   const start = Date.now();
 
-  const client = new OpenAI({
-    apiKey: process.env.GROQ_API_KEY,
-    baseURL: "https://api.groq.com/openai/v1",
-  });
-
+  // Request the Groq API for the response based on the prompt
   try {
     const response = await client.chat.completions.create({
       model: "gemma2-9b-it",
